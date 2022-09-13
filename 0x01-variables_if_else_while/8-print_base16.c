838f9c85cdb67c5314b841fc575ae8da829aca1f
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-13 09:22:37 -0700

    done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+char le;
+for (ch = 0; ch < 10; ch++)
+{
+putchar((ch % 10) + '0');
+}
+for(le = 'a'; le <= 'f'; let++)
+putchar(le);
+putchar('\n');
+
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c~
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 0; ch < 10; ch++)
+{
+putchar((ch % 10) + '0');
+}
+putchar('\n');
+
+return (0);
+}
