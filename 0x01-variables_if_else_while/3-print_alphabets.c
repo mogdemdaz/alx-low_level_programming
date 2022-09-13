2aaefb1f026e8b6109cf13280ea1ff853d7d72bf
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-13 07:34:35 -0700

    added 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+for (ch = 'A'; ch <= 'Z'; ch++)
+putchar(ch);
+putchar ('\n');
+
+return (0);
+}
