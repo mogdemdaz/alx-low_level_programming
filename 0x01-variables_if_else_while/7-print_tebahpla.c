677f3cde30bf6b33912f0fc22843d9ab535e0700
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-13 09:05:10 -0700

    added 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+putchar(ch);
+}
+putchar ('\n');
+
+return (0);
+}
