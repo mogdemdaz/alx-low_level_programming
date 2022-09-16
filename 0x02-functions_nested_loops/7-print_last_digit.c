4fcb6816453257845789ca45fb27747e5283017b
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-16 13:28:30 -0700

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - code to print last digit
+ * @n: parameter n
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+int n, ld;
+ld = n % 10;
+return (ld); 
+}
