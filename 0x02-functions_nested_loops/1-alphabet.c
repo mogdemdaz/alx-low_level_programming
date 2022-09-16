bb6ec455cbf465ae1ac6c36400dbfca8e1a0bb73
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-16 05:08:22 -0700

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,7 +6,7 @@
  * Return: Always 0.
  */
 
-int print_alphabet(void)
+void print_alphabet(void)
 {
 char ch;  
 for (ch = 'a'; ch <= 'z'; ch++)
@@ -14,5 +14,4 @@ for (ch = 'a'; ch <= 'z'; ch++)
 ptchar(ch);
 }
 ptchar('\n');
-return (0); 
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,12 +5,4 @@ void ptchar(char c)
 putchar(c);
 }
 
-void print_alphabet(void)
-{
-char ch;  
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-ptchar(ch);
-}
-ptchar('\n');
-}  
+void print_alphabet(void);
