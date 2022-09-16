f03c5fb512db0ee38462b5ef178862ec0d364133
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-16 13:19:25 -0700

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - code to print absolute numbers
+ * @c: The new color code.
+ * Return: Always 0.
+ */
+int _abs(int c)
+{
+if (c > 0)
+_putchar(c);
+else
+_putchar(-c);
+return (0);
+}
