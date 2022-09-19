08823f7f7eb7c5160077fa2c9e3c1cfbabbac4da
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-19 02:03:52 -0700

    really hard lol

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,20 +13,41 @@ if (n < 98)
 {
 for (num = n; num <= 98; num++)
 {
+if (num < 9)
 _putchar(num + '0');
+
+else
+{
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
 _putchar(',');
 _putchar(' ');
 }
 }
 
 else if (n == 98)
-_putchar((n + '0'));
+{
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
 
 else if (n > 98)
 {
 for (num = n; num >= 98; num--)
 {
-_putchar(num + '0');
+if (n >= 100)
+{
+_putchar((num / 100) + '0');
+_putchar(((num / 10) % 10) + '0');
+_putchar((num % 100) + '0');
+}
+
+else
+{
+_putchar((num / 10) + '0');
+_putchar((num % 10) + '0');
+}
 _putchar(',');
 _putchar(' ');
 }
