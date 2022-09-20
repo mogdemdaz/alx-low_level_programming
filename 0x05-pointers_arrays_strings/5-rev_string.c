3c7747cff35db09f298649f6c618ffd076c19cd3
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 09:07:46 -0700

    how

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,13 +6,13 @@
  */
 void rev_string(char *s)
 {
-int i =0, sum = 0;
+int i = 0, sum = 0;
 char tmp = *s;
 while (s[i++])
 {
 sum++;
 }
-for (i = sum - 1; i >= 0; i--)OB
+for (i = sum - 1; i >= 0; i--)
 tmp[i] = s[i]);
 
 s = tmp;
