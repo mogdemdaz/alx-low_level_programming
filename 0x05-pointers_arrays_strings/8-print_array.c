41e57226007f706dac44c1e6d698a869de4dfc6d
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 10:37:59 -0700

    done

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_array - check the code for
+ * @a: parameter 1
+ * @n: Paremeter 2
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+int i;
+for(i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n - 1)
+continue;
+printf(", ");
+}
+printf("\n");
+}
