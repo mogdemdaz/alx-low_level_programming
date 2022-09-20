7caf91095d5cec24b246a028e8b1f9b459ca7190
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 10:51:38 -0700

    done

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - check the code
+ * @dest: parameter 1
+ * @src: parameter 2
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
