9a89f98f3ab7c0e7ca9b1b43d55dbe80bdadb88c
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 07:36:51 -0700

    done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ * @str: parameter
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+while(*str)
+{
+_putchar(*str);
+*str++;
+}
+_putchar('\n');
+}
