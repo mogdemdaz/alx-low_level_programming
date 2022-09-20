af7240656f53aecdcb99093b6ea3436a33d4e7df
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 06:53:25 -0700

    done

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,16 @@
 #include "main.h"
 /**
  * swap_int - check the code
- *
+ * @a: first integer
+ * @b: Second integer
  * Always 0.
  */
 
 
 void swap_int(int *a, int *b)
 {
-
+int *c;
+*c = *a;
+*a = *b;
+*b = *c;
 }
