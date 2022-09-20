0e0b8a57e0352305b69f5801bf6d67d574c7d2ef
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 16:54:28 -0700

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,10 @@ for (i = 1; i <= 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j < 10)
-_putchar(i + '0');
-else
-{
+if ( j > 9)
 _putchar((i / 10) + '0');
+ 
 _putchar((i % 10) + '0');
-}
 }
 _putchar('\n');
 }
