a7a82111e1ccc0402b48cf1f3fc1146d476cfd86
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 08:25:25 -0700

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,14 +2,16 @@
 
 /**
  * print_rev - check the code
- * @s: parameter
+ * @so: parameter
  * Return: Always 0.
  */
-void print_rev(char *s)
+void print_rev(char *so)
 {
 int i, sum = 0;
-while (*s[i++])
+char s = *so;
+while (s)
 {
+s++;
 sum++;
 }
 for (i = sum - 1; i >= 0; i--)
