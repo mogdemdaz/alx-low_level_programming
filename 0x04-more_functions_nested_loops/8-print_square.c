cf6838826305a4f88e845d168dab4e24e7e9df15
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-20 17:19:11 -0700

    done

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Draws a square.
+ * @size: parameter
+ */
+void print_square(int size)
+{
+int height, width;
+if sizen > 0)
+{
+for (height = 0; height < size; height++)
+{
+for (width = 0; width < size; width++)
+_putchar('#');
+if (height == size - 1)
+continue;
+_putchar('\n');
+}
+}
+_putchar('\n');
+}
