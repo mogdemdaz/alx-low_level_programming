ae3c73a6caac78ee65b195934b87acdbf74b0f0a
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-22 16:33:22 -0700

    done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * _strcat - check the code
+ * @dest: params 1
+ * @src: params 2
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
