1dc3ac2ba3467ee1cda33fc4d22f96c2fdb27579
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-23 11:46:53 -0700

    done 2

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ * @n: params 1
+ * Return: Always 0.
+ */
+char *cap_string(char *n)
+{int index = 0;
+
+while (str[index])
+{
+while (!(str[index] >= 'a' && str[index] <= 'z'))
+index++;
+
+if (str[index - 1] == ' ' ||
+str[index - 1] == '\t' ||
+str[index - 1] == '\n' ||
+str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+str[index] -= 32;
+index++;
+}
+
+return (str);
+
+}
