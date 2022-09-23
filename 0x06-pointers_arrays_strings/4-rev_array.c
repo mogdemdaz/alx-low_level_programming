a8542688f8605a9808a5783d156a281fae18d933
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-23 10:28:17 -0700

    done

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - check the code
+ * @a: params 1
+ * @n: params 2
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+int i, temp;
+for (i = n; i >= n / 2; i--)
+{
+temp = a[n - 1 - i];
+a[n - 1 - i] = a[i];
+a[i] = temp;
+}
+}
