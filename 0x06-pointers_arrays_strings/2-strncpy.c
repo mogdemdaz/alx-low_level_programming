7e2aba59bd16fddcae0ddb2ccf8403449e635f65
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-23 08:23:13 -0700

    done

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,8 +12,7 @@ char *_strncpy(char *dest, char *src, int n)
 int i, count = 0;
 for (i = 0; i < n && src[i] != '\0'; i++)
 dest[i] = src[i];
-for ( ; i < n; i++)
-dest[i] = '\0';
+
 
 return (dest);
 }
