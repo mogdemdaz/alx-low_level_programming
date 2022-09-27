4985b5be91e319145d703b6142e54c283869fb86
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-27 13:26:42 -0700

    corrected return type of 0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,7 +10,7 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0, count = 0;
+unsigned int i = 0, count = 0;
 
 while (s[i++])
 count++;
@@ -20,5 +20,5 @@ for (i = 0; i < n; i++)
 s[i] = b;
 }
 
-return (*s);
+return (s);
 }
