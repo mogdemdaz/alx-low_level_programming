e2f9a96f71f18fc2e6f58e14478b28a2c1cf0bd3
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-09-28 13:32:59 -0700

    added 4-strpbrk.c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strpbrk - locates a character @c in a string @s
+ *
+ * @s: string to search
+ * @accept: character to locate
+ *
+ * Return: A pointer to the first occurence of @accept in @s
+ */
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+char *p;
+for (i = 0; s[i] >= '\0'; i++)
+{
+if (c == s[i])
+p = &(s+i);
+return (p);
+}
+return ('\0');
+}
