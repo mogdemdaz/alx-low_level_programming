20366e4f25f8603e1344fdb26d9d40ee47ebd4f6
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-05 07:08:27 -0700

    added 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to work on
+ *
+ * Return: the lenght of the string @s
+ */
+
+int _strlen_recursion(char *s)
+{
+int sum = 0;
+if (*s)
+{
+sum++;
+sum += _strlen_recursion(s + 1);
+}
+return (sum)
+}
