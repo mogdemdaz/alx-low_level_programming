1b67cebaa5dfcc815e8ef1a785b095971a4a242f
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-05 14:38:29 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  main - check the code
+ *  @argc: argument count
+ *  @argv: argument value
+ *  Return - value to be returned
+ */
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[argc - 1]);
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+
+int _putchar(char c);
+
+
+
+#endif
