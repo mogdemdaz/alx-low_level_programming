4b69b801bc2b5e42100e3807f4ac45f9c99e8a93
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-05 15:10:26 -0700

    added 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: value to be returned
+ */
+int main(int argc, char *argv[])
+{
+int num, digit, sum = 0;
+for (num = 1; num < argc; num++)
+{
+for (digit = 0; argv[num][digit]; digit++)
+{
+if (argv[num][digit] < '0' || argv[num][digit] > '9')
+{
+printf("Error\n");
+return (1);
+}
+}
+sum += atoi(argv[num]);
+}
+printf("%d\n", sum);
+return (0);
+}
