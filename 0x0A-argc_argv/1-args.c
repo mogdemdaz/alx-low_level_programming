af48f2c6caa9692eda38bec7459d74c7353ee43d
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-05 14:49:08 -0700

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: value to be returned
+ */
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", (argc - 1));
+return (0);
+}
