21abd88d95396b9cf7500ec2a5bb47566d75354a
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-05 01:40:52 -0700

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string, followed by a new line
+ *
+ * @s: string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s + 1);
+_putchar (*s);
+}
+else
+_putchar ('\n');
+}
