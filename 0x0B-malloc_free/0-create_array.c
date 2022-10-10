aba754ddb4d5e82d4e1bd91441e14e9d37ae0a62
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-10 13:15:20 -0700

    removed mixed declarations and code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,13 +9,14 @@
  */
 char *create_array(unsigned int size, char c)
 {
+char *array;
+unsigned int i;
+
 if (size == 0)
 return (NULL);
 
-char *array;
 array = malloc(sizeof(char) * size);
 
-unsigned int i;
 for (i = 0; i < size; i++)
 array[i] = c;
 
