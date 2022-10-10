b82118cfdcccf0c2dfb25087e36321c75f410490
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-10 13:42:35 -0700

    compleed task 2

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup - returns a pointer to a new string which is a duplicate of @str
+ * @str: parameter to duplicate.
+ * Return: pointer to the newly allocated space or NULL
+ */
+
+char *_strdup(char *str)
+{
+char *copy;
+int i, count = 0;
+
+if (str == NULL)
+return (NULL);
+
+while (*str)
+count++;
+
+copy = malloc(sizeof(char) * count);
+
+if (copy == NULL)
+return (NULL);
+
+for (i = 0; str[i]; i++)
+copy[i] = str[i];
+
+copy[count] = '\0';
+return (copy);
+
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,9 +2,11 @@
 #define HEADER_FILE
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.hi>
 
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
 
 #endif
