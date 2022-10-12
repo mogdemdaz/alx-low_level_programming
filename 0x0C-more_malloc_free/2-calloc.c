6cf51477b1262155f1efe994c45e4269267db0a1
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-12 01:38:00 -0700

    completed 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory for an arry using malloc
+ * @nmemb: number of array elements
+ * @size: bytes  of each element
+ * Return: a pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *mem;
+char *filler;
+unsigned int index;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+mem = malloc(size * nmemb);
+
+if (mem == NULL)
+return (NULL);
+filler = mem;
+
+for (index = 0; index < (size * nmemb); index++)
+filler[index] = '\0';
+
+return (mem);
+}
