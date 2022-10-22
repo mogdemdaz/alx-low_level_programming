cb6e70a80344433914af72d9e65ff53fdc3c1049
Aziagba Chukwuemeka <emeka.aziagba@gmail.com>
2022-10-22 14:25:24 -0700

    added 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - print strings
+ * @separator: string separator
+ * @n: constant number of variable params
+ * @...: variable params
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list strings;
+char *str;
+unsigned int i;
+
+va_start(strings, n);
+
+for (i = 0; i < n; i++)
+{
+str = va_arg(strings, char *);
+
+if (str == NULL)
+printf("(nil)");
+else
+printf("%s", str);
+if (i != (n - 1) && separator != NULL)
+printf("%s", separator);
+}
+
+printf("\n");
+
+va_end(strings);
+}
